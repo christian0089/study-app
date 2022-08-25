09c1f4f175c8f05f1269b2c0af9a1825967d8513
j_dev <j_developer@naver.com>
2022-08-25 20:50:07 +0900

    add encryptor

---
+++ src/main/java/com/study/app/Test.java
@@ -0,0 +1,10 @@
+package com.study.app;
+
+import com.study.app.common.Encryptor;
+
+public class Test {
+
+	public static void main(String[] args) throws Exception  {
+		System.out.println( Encryptor.encrypt( "AAA" ) );
+	}
+}
