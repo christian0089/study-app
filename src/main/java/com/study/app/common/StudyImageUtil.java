2d05c38ae906fee3ebf558ac9b54e529085db6db
j_dev <j_developer@naver.com>
2022-09-15 22:11:59 +0900

    fix

--- src/main/java/com/study/app/common/StudyImageUtil.java
+++ src/main/java/com/study/app/common/StudyImageUtil.java
@@ -26,15 +26,11 @@ public class StudyImageUtil {
 	@Value("${img.pay.base.url}")
 	private String payBaseUrl;
 	
-	public String test() {
-		return this.payBaseUrl;
-	}
-	
 	private final String BUCKET_REP_SUFFIX = "/rep";
 	private final String BUCKET_STORY_SUFFIX = "/story";
 	
 	/**
-	 * 버킷 대표이미지 파일 업로드
+	 * 버킷 대표 이미지 파일 업로드
 	 * 
 	 * @param file
 	 * @return
@@ -85,5 +81,32 @@ public class StudyImageUtil {
 		file.transferTo( new File( this.bucketUploadBasePath + this.BUCKET_STORY_SUFFIX + monthSuffix + saveFileName ) );
 		
 		return this.bucketBaseUrl + this.BUCKET_STORY_SUFFIX + monthSuffix + saveFileName;
+	}
+	
+	/**
+	 * 지출 이미지 파일 업로드
+	 * 
+	 * @param file
+	 * @return
+	 * @throws Exception
+	 */
+	public String uplaodPayImage( MultipartFile file ) throws Exception {
+		if( file.getSize() > imgMaxSize ) {
+			throw new StudyException( "1001", "업로드 파일의 용량이 초과되었습니다. [Max 10Mb]" );
+		}
+		String uploadFileName = file.getOriginalFilename();
+		String uploadFileFormat = uploadFileName.substring( uploadFileName.lastIndexOf(".") +1 );
+		
+		// 파일 확장자 검증
+		if( !CommonUtil.isAcceptedUploadFileFormat( uploadFileFormat ) ) {
+			throw new StudyException( "1002", "[" + uploadFileFormat + "] 형식의 파일은 업로드 할 수 없습니다." );
+		}
+		
+		String monthSuffix = CommonUtil.SEPARATOR + CommonUtil.getServerTime( "%Y%m" );
+		String saveFileName = CommonUtil.SEPARATOR + CommonUtil.getRandomString( -1, 8 ) + CommonUtil.getServerTime( "%Y%m%d" ) + CommonUtil.getRandomString( -1, 8 ) + "." + uploadFileFormat;
+		
+		file.transferTo( new File( this.payUploadBasePath + monthSuffix + saveFileName ) );
+		
+		return this.payBaseUrl + monthSuffix + saveFileName;
 	}
 }
